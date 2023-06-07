4ad20cab987948ca942c236bf941a88ff4066582
YuliaMoore <115689321+YuliaMoore@users.noreply.github.com>
2023-06-07 22:37:23 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,32 +5,39 @@ export class App extends Component {
     contacts: [],
     name: '',
   };
+  handleChange = e -> {
+    const { contacts, name } = e.currentTarget;
+    this.setState({contacts, name})
+}
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
 
   render() {
     return (
-      <input
-        type="text"
-        name="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        required
-      />
+      <div>
+        <h1>Phonebook</h1>
+        <form>
+          <h2>Name</h2>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <h2>Number</h2>
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+          <button type="submit">Add contact</button>
+        </form>
+      </div>
     );
   }
 }
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101',
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
